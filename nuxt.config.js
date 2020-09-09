1965fd51a23a6c5352360c6270baa2421d33126d
unknown <r.kashifshahzad@outlook.com>
2020-09-09 23:51:56 +0500

    basic1

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: "NuxtJs - Cool Tutorial",
+    title: process.env.npm_package_name || "",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
