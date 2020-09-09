90b24007cb4133b118979815cb3f82ae5b7858da
unknown <r.kashifshahzad@outlook.com>
2020-09-09 23:48:48 +0500

    basic

---
+++ pages/videos/_id/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    We are in a video {{ video.name}}
+    <nuxt-link :to="`/videos/${video.id}/edit`">
+      <button>Edit</button>
+    </nuxt-link>
+    <nuxt-link :to="`/videos`">
+      <button>Go Back</button>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["video"],
+};
+</script>
+
+<style>
+</style>
